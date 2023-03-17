015d66cb0c80ee168addc7a2f20cf77c574287ce
hopelesssoulx <hopelesssoulx@gmail.com>
2023-03-17 11:05:14 +0800

    p77

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+
+const cors = require('cors')
+app.use(cors())
+
+
+app.use(express.urlencoded({ extended: false }))        // 仅解析 application/x-www-form-urlencoded
+
+
+app.listen(3000, function () {
+    console.log('api server running at http://127.0.0.1:3000')
+})
