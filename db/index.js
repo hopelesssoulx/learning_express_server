7add1c9c2898ce3f6de600e6cb63e32fd89a2b9d
hopelesssoulx <hopelesssoulx@gmail.com>
2023-03-17 11:40:46 +0800

    p79

---
+++ db/index.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql')
+
+
+const db = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '123456',
+    database: 'learning_express_server',
+})
+
+
+module.exports = db
--- router_handler/user.js
+++ router_handler/user.js
@@ -1,9 +1,45 @@
 // 在此文件定义和用户相关的路由处理函数，供 /router/user.js 模块进行调用
 
 
+const db = require('../db/index')
+const bcrypt = require('bcryptjs')
+
+
 // 注册
 exports.register = (req, res) => {
-    res.send('register OK')
+    // 判断数据是否存在
+    const userInfo = req.body
+    if (!userInfo.username || !userInfo.password) {
+        return res.send({ status: 1, message: '用户名或密码不能为空！' })
+    }
+
+    // 判断用户是否存在
+    const sql1 = `select * from ev_users where username=?`
+    db.query(sql1, [userInfo.username], function (e, rs) {
+        if (e) {
+            return res.send({ status: 1, message: e.message })
+        }
+        if (rs.length > 0) {
+            return res.send({ status: 1, message: '用户名被占用，请更换其他用户名！' })
+        }
+
+        // 写入数据
+        userInfo.password = bcrypt.hashSync(userInfo.password, 10)
+        const sql2 = 'insert into ev_users set ?'
+        db.query(
+            sql2, {
+            username: userInfo.username,
+            password: userInfo.password
+        }, function (e, rs) {
+            if (e) {
+                return res.send({ status: 1, message: e.message })
+            }
+            if (rs.affectedRows !== 1) {
+                return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })
+            }
+            res.send({ status: 0, message: '注册成功！' })
+        })
+    })
 }
 
 // 登录
