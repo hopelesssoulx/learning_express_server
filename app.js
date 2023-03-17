cc4016142dd261e164f203476b0f38b9d8ba5cf2
hopelesssoulx <hopelesssoulx@gmail.com>
2023-03-17 12:00:23 +0800

    p80_p1

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.use(cors())
 app.use(express.urlencoded({ extended: false }))        // 仅解析 application/x-www-form-urlencoded
 
 
+// 在路由前声明
+// 响应数据的中间件
+app.use(function (req, res, next) {
+    // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
+    res.cc = function (e, status = 1) {
+        res.send({
+            // 状态
+            status,
+            // 状态描述，判断 err 是 错误对象 还是 字符串
+            message: e instanceof Error ? e.message : e,
+        })
+    }
+    next()
+})
+
+
 const userRouter = require('./router/user')
 app.use('/api', userRouter)
 
--- router_handler/user.js
+++ router_handler/user.js
@@ -17,10 +17,12 @@ exports.register = (req, res) => {
     const sql1 = `select * from ev_users where username=?`
     db.query(sql1, [userInfo.username], function (e, rs) {
         if (e) {
-            return res.send({ status: 1, message: e.message })
+            // return res.send({ status: 1, message: e.message })
+            return res.cc(e)
         }
         if (rs.length > 0) {
-            return res.send({ status: 1, message: '用户名被占用，请更换其他用户名！' })
+            // return res.send({ status: 1, message: '用户名被占用，请更换其他用户名！' })
+            return res.cc('用户名被占用，请更换其他用户名！')
         }
 
         // 写入数据
@@ -32,12 +34,15 @@ exports.register = (req, res) => {
             password: userInfo.password
         }, function (e, rs) {
             if (e) {
-                return res.send({ status: 1, message: e.message })
+                // return res.send({ status: 1, message: e.message })
+                return res.cc(e)
             }
             if (rs.affectedRows !== 1) {
-                return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })
+                // return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })
+                return res.cc('注册用户失败，请稍后再试！')
             }
-            res.send({ status: 0, message: '注册成功！' })
+            // res.send({ status: 0, message: '注册成功！' })
+            return res.cc('注册成功！', 0)
         })
     })
 }
