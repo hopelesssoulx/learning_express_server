f0a08cdc7b89132412b618c54b4e1e4b6a727b34
hopelesssoulx <hopelesssoulx@gmail.com>
2023-03-18 18:11:20 +0800

    p90

--- router/artcate.js
+++ router/artcate.js
@@ -5,8 +5,15 @@ const router = express.Router()
 const artCate_handler = require('../router_handler/artcate')
 
 
+const expressJoi = require('@escook/express-joi')
+const { add_cate_schema } = require('../schema/artcate')
+
+
 // 获取文章分类数据
 router.get('/cates', artCate_handler.getArtCates)
+
+// 新增文章分类
+router.post('/addcates', expressJoi(add_cate_schema), artCate_handler.addArtCates)
 
 
 module.exports = router
--- router_handler/artcate.js
+++ router_handler/artcate.js
@@ -16,3 +16,45 @@ exports.getArtCates = (req, res) => {
         })
     })
 }
+
+// 新增文章分类
+exports.addArtCates = (req, res) => {
+    const sql = 'select * from ev_article_cate where name=? or alias=?'
+    db.query(
+        sql, [
+        req.body.name,
+        req.body.alias],
+        (e, rs) => {
+            if (e) {
+                return res.cc(e)
+            }
+
+            if (rs.length === 2) {
+                return res.cc('分类名称和分类别名被占用')
+            }
+
+            if (rs.length === 1 && rs[0].name === req.body.name && rs[0].alias === req.body.alias) {
+                return res.cc('分类名称和分类别名被占用')
+            }
+            if (rs.length === 1 && rs[0].name === req.body.name) {
+                return res.cc('分类名称被占用')
+            }
+            if (rs.length === 1 && rs[0].alias === req.body.alias) {
+                return res.cc('分类别名被占用')
+            }
+
+            // 写入
+            const sql = 'insert into ev_article_cate set ?'
+            db.query(sql, req.body, (e, rs) => {
+                if (e) {
+                    return res.cc(err)
+                }
+                if (rs.affectedRows !== 1) {
+                    return res.cc('新增文章分类失败')
+                }
+
+                res.cc('新增文章分类成功')
+            })
+        }
+    )
+}
